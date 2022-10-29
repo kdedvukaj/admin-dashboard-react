f1d7e486a3cf8fe708164dacd9825ce0b783c743
kdedvukaj <kdedvukaj94@gmail.com>
2022-10-30 00:47:29 +0200

    React Route config

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import { Routes, Route } from "react-router-dom";
+
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import List from "./pages/list/List";
+import Single from "./pages/single/Single";
+import New from "./pages/new/New";
 
 function App() {
   return (
     <div className="container">
-      <Home />
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="librarians">
+            <Route index element={<List />} />
+            <Route path=":librarianId" element={<Single />} />
+            <Route path="new" element={<New />} />
+          </Route>
+          <Route path="students">
+            <Route index element={<List />} />
+            <Route path=":studentId" element={<Single />} />
+            <Route path="new" element={<New />} />
+          </Route>
+          <Route path="books">
+            <Route index element={<List />} />
+            <Route path=":bookId" element={<Single />} />
+            <Route path="new" element={<New />} />
+          </Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
