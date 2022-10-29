4b205a9a7198b065fb32d5f5d53a38045f134b31
kdedvukaj <kdedvukaj94@gmail.com>
2022-10-30 00:27:21 +0200

    Basic Pages Added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Home from "./pages/home/Home";
+
 function App() {
-  return <div>Online React Library</div>;
+  return (
+    <div className="container">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
 const Home = () => {
-  return <div></div>;
+  return <div className="container">Hi</div>;
 };
 
 export default Home;
